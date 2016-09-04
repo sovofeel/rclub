9f1109f7bfbf800f20de3f1228ae04de2b75f2c5
olganagorna <olechka.nagorna@gmail.com>
2016-09-05 02:06:14 +0300

    menu, gallery

---
+++ assets/source/scripts/header.js
@@ -0,0 +1,45 @@
+var heightForFixHeader = $('header').offset().top + $('header').outerHeight();
+
+$(window).scroll(function (event) {
+    if( $(window).scrollTop() > $('.contact').offset().top && 
+    !($('.contact').hasClass('fixed'))){
+    	$('.contact').addClass('fixed');
+    };
+
+    if( $(window).scrollTop() == 0 && 
+    ($('.contact').hasClass('fixed'))){
+    	$('.contact').removeClass('fixed');
+    };
+
+    var headroom  = new Headroom(document.querySelector("header"));
+		
+
+    if($(window).scrollTop() > heightForFixHeader)	{
+			$("header").addClass('headroom--pinned');
+			headroom.init();
+    } else{
+    	$("header").removeClass('headroom--pinned');
+    	headroom.destroy();
+    };
+
+    if( $(window).scrollTop() > $('.gallery').offset().top - $('.gallery').innerHeight()){
+    	$('.gallery').addClass('show_slide3');
+    	setTimeout(function(){
+    		$('.gallery').addClass('color_slide3');
+    	}, 1500);
+    };
+
+    if( $(window).scrollTop() > $('.gallery').offset().top - $('.gallery').innerHeight()/2){
+    	$('.gallery').addClass('show_slide2');
+    	setTimeout(function(){
+    		$('.gallery').addClass('color_slide2');
+    	}, 1500);
+    };
+
+    if( $(window).scrollTop() > $('.gallery').offset().top){
+    	$('.gallery').addClass('show_slide1');
+    	setTimeout(function(){
+    		$('.gallery').addClass('color_slide1');
+    	}, 1500);
+    }
+});
