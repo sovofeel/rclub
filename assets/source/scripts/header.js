8d3be3d48531038bd3dc30d53a9b0726260ed00a
olganagorna <olechka.nagorna@gmail.com>
2016-09-16 09:01:20 +0300

    gallery

--- assets/source/scripts/header.js
+++ assets/source/scripts/header.js
@@ -39,7 +39,7 @@ $(document).ready(function() {
                 
             // };
         }
-        
+
         // var headroom = new Headroom(document.querySelector("header"),{
         //   "offset": heightForFixHeader,
         //   "tolerance": 5
@@ -63,6 +63,42 @@ $(document).ready(function() {
 
 
     });
+
+    // left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+function preventDefault(e) {
+  e = e || window.event;
+  if (e.preventDefault)
+      e.preventDefault();
+  e.returnValue = false;  
+}
+
+function preventDefaultForScrollKeys(e) {
+    if (keys[e.keyCode]) {
+        preventDefault(e);
+        return false;
+    }
+}
+
+function disableScroll() {
+  if (window.addEventListener) // older FF
+      window.addEventListener('DOMMouseScroll', preventDefault, false);
+  window.onwheel = preventDefault; // modern standard
+  window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
+  window.ontouchmove  = preventDefault; // mobile
+  document.onkeydown  = preventDefaultForScrollKeys;
+}
+
+function enableScroll() {
+    if (window.removeEventListener)
+        window.removeEventListener('DOMMouseScroll', preventDefault, false);
+    window.onmousewheel = document.onmousewheel = null; 
+    window.onwheel = null; 
+    window.ontouchmove = null;  
+    document.onkeydown = null;  
+}
 
     $(".hover_part").mouseover(function() {
         var slideIndex = $(this).closest('.hover_part').index('.hover_part') + 1;
