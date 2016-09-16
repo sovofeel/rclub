7e102549713bf202d02abd4a34f98559dbe2da25
Sovofeel <sovofeel@gmail.com>
2016-09-17 04:55:51 +0500

    stiky sidebar alpha version

--- assets/js/events.js
+++ assets/js/events.js
@@ -1,6 +1,34 @@
 $(document).ready(function() {
 
-    
+    var $sidebar = $('.js-sidebar'), sidePos, winPos, sideHeight, slideHeight, sideOffsetRight, vh80;
 
-    
+
+
+    function refreshVar() {
+        vh80 = $(window).height() / 100 * 90;
+        slideHeight = $('.fotorama').height();
+        sidePos = $sidebar.offset().top + $(window).height() + vh80;
+        sideHeight = $sidebar.outerHeight(true);
+        sideOffsetRight = $(window).width() - $sidebar.offset().left - $sidebar.outerWidth();
+
+    }
+
+    refreshVar();
+    $(window).resize(refreshVar);
+    $(window).scroll(function() {
+        winPos = $(window).scrollTop();
+
+        console.log(winPos);
+
+
+        if (winPos >= vh80) {
+            $sidebar.addClass('fixed');
+            $sidebar.css('right', sideOffsetRight)
+        }
+        else {
+            $('.js-sidebar').removeClass('fixed ');
+            $sidebar.css('right', 0)
+        }
+    });
+
 });
